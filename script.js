21b824080d9c45ac43071d068e322cab937283b2
Sean Hammell <sean.m.hammell@gmail.com>
2022-02-09 09:10:39 -0500

    Add button to reset board
    "Clear Board" button resets filled grid boxes and prompts
    user to enter the number of squares they would like per side.

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-function main() {
-    const container = document.querySelector(".grid-container");
+const gridSize = 800;
 
-    for (let i = 0; i < 16; i++) {
+function resetBoard() {
+    const squaresPerSide = prompt("How many squares would you like per side?");
+    drawBoard(squaresPerSide);
+}
+
+function drawBoard(squaresPerSide) {    
+    const container = document.querySelector(".grid-container");
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+
+    for (let i = 0; i < squaresPerSide; i++) {
         const rowContainer = document.createElement("div");
         rowContainer.classList.add("row-container");
         console.log(rowContainer);
 
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < squaresPerSide; j++) {
             const gridBox = document.createElement("div");
             gridBox.classList.add("grid-box");
             gridBox.setAttribute("id", `${16 * i + j}`);
+            gridBox.style.height = `${gridSize / squaresPerSide}px`;
+            gridBox.style.width = `${gridSize / squaresPerSide}px`;
 
             gridBox.addEventListener("mouseover", () => {
                 gridBox.classList.toggle("fill");
@@ -20,6 +32,10 @@ function main() {
 
         container.appendChild(rowContainer);
     }
+}
+
+function main() {
+    drawBoard(16);
 }
 
 main();
